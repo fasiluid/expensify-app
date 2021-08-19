5bd0bab6e1ae29a4ef58725a4b2c5f2d8aedeeed
fasil.uid <fasil.uid@gmail.com>
2021-08-19 21:01:35 +0530

    Remove dummy expenses

--- src/app.js
+++ src/app.js
@@ -4,42 +4,11 @@ import { Provider } from 'react-redux';
 
 import AppRouter from "./routers/AppRouter";
 import configureStore from "./store/configureStore";
-import { addExpense } from './actions/expenses';
-import { setFilterText } from './actions/filters';
-import getVisibleExpenses from "./selectors/expenses"
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 import "react-dates/lib/css/_datepicker.css";
 
 const store = configureStore();
-
-store.dispatch(
-    addExpense({
-        description: "Water Bill",
-        amount: 300,
-        createdAt: 4500
-    })
-);
-
-store.dispatch(
-    addExpense({
-        description: "Gas Bill",
-        amount: 200,
-        createdAt: 1000
-    })
-);
-
-store.dispatch(
-    addExpense({
-        description: "Rent",
-        amount: 100,
-        createdAt: 109500
-    })
-);
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
 
 const jsx = (
     <Provider store={store}>
